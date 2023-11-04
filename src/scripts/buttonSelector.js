a9d535f03a1723bf61f2a0bd1b5f9b6970ce05b2
Taylor the Diabetic <107991832+TheWiredDiabetic@users.noreply.github.com>
2023-11-04 03:01:58 +0000

    Test #1 - Rolling out update for non-hidden components w/ test load audio JS

--- src/scripts/buttonSelector.js
+++ src/scripts/buttonSelector.js
@@ -1,13 +1,26 @@
 const toggleButton = document.getElementById('toggle-audio-evercast');
-const audioControls = document.getElementById('audio-evercast');
-                
+const audioControls = document.getElementById('evercastPlayerNormal');
+const evercastMain = document.getElementById('evercastPlayerNormal')
+
 toggleButton.addEventListener('click', () => {
-     if (audioControls.style.display === 'none') {
+     if (evercastMain.style.display === 'none') {
          audioControls.style.display = 'block';
-         toggleButton.innerText = 'Hide Audio Controls';
+         toggleButton.innerText = 'Hide Evercast Player';
      } else {
-        audioControls.style.display = 'none';
-        toggleButton.innerText = 'Show Audio Controls';
+        evercastMain.style.display = 'none';
+        toggleButton.innerText = 'Show Evercast Player';
     }
 });
 
+const audioUrlPostInputA = document.getElementById('submitEvercastUrlStreamer1')
+const audioUrlPassThroughSubmit = document.getElementById('passUrlSubmit1')
+
+audioUrlPassThroughSubmit.addEventListener('click', () => {
+    const AudioSrc = audioUrlPostInputA.setAttribute('src', 'AUDIO_SRC_FILE');
+    function cs_change_music(music) {
+        audioControls.onpause();
+        audioControls.setAttribute('src', audioUrlPostInputA.innerText)
+        audioControls.load();
+        audioControls.onplay();
+    }
+})
